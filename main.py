1432c16b52daf411ef1fb8a9b460c168d2e58118
Daniela Rojas <rojasbdaniela30@gmail.com>
2021-12-01 21:06:01 -0500

    Initialization

---
+++ main.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import json
+import time
+
+option = webdriver.ChromeOptions()
+option.add_argument("--incognito")
+
+driver = webdriver.Chrome("./chromedriver/chromedriver.exe", chrome_options=option)
+driver.get("https://www.instagram.com/p/CWeGL2NPaNz/")
+driver.maximize_window()
+time.sleep(10)
+
+body = driver.execute_script("return document.body")
+source = body.get_attribute('innerHTML') 
+
+soup = BeautifulSoup(source, "html.parser")
+data = soup.find('script')
+data= str(data)
+data=data.replace('<script type="text/javascript">window._sharedData = ','')
+data=data.replace(';</script>','')
+
+jasonData=json.loads(str(data))
+file1 = open("./scrappingFiles/content1.json","w", encoding='utf-8') 
+comments =jasonData['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_media_to_parent_comment']['edges']
+print(len(comments))
+for i in range (len(comments)):
+    file1.write(comments[i]['node']['text']+"\n")
+
+file1.close()
+driver.close()
